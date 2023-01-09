4d0753d63815a3a04a998740561b59cc812282af
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-09 18:47:32 -0500

    user chosen color

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 //game pattern var to keep track of colors selected 
 let gamePattern = [];
 
+
+
 //function to get a random number
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
@@ -16,9 +18,16 @@ let randomChosenColor = buttonColors[nextSequence()]
 //add the accuring pattern to the game pattern array to keep track
 gamePattern.push(randomChosenColor)
 
-//select a random color and animate it to flash and play audio
-$(`#${randomChosenColor}`).fadeOut(100).fadeIn(100).ready(() => {
-    const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-    audio.play();
-})
+//select a random color and animate it to flash 
+$(`#${randomChosenColor}`).fadeOut(100).fadeIn(100)
 
+//play audio
+let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+audio.play();
+
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+
+$(".btn").click((event) => {
+    let userChosenColor = event.currentTarget.id;
+    console.log(userChosenColor)
+})
