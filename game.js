386deebb5e6f7d4e0c3d59895bb4765c5d87779a
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-09 19:20:28 -0500

    click sounds working

--- game.js
+++ game.js
@@ -6,24 +6,25 @@ let gamePattern = [];
 
 let userClickedPattern = []
 
+
 //function to get a random number
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber
+    
+    //how to choose a random color
+    let randomChosenColor = buttonColors[randomNumber]
+
+    //add the accuring pattern to the game pattern array to keep track
+    gamePattern.push(randomChosenColor)
+
+    //select a random color and animate it to flash 
+    let name = $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100)
+
+    //play audio
+    playSound(name)
 }
 
-//how to choose a random color
-let randomChosenColor = buttonColors[nextSequence()]
 
-//add the accuring pattern to the game pattern array to keep track
-gamePattern.push(randomChosenColor)
-
-//select a random color and animate it to flash 
-$(`#${randomChosenColor}`).fadeOut(100).fadeIn(100)
-
-//play audio
-let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-audio.play();
 
 //detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click((event) => {
@@ -31,4 +32,13 @@ $(".btn").click((event) => {
     
     //push user clicked color into the user clicked pattern
     userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor)
+
+    console.log(userClickedPattern)
 })
+
+function playSound(name){
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
